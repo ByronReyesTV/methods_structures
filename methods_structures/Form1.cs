5b8c80fa6e2be67e4dceeb2f1443266caa27797e
reyes221093 <102719907+reyes221093@users.noreply.github.com>
2022-05-20 14:13:36 +0800

    Add project files.

---
+++ methods_structures/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace methods_structures
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialogPhoto.Filter = "image files (*.jpg; *.jpeg; *.png;)|*.jpg; *.jpeg; *.png";
+            if (openFileDialogPhoto.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialogPhoto.FileName);   
+                textBox1.Text = openFileDialogPhoto.FileName;
+            }
+            openFileDialogPhoto.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            openFileDialogPhoto.ShowDialog();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
